c9cd749c1af0153ed7389bac5a167d5211bae003
Iyalla-D <iyalladinney@gmail.com>
2023-04-29 01:09:08 +0100

    made changes to allow sending of cached data

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'public, max-age=86400'); 
+  next();
+});
+
 app.get('/api/country/:country', async (req, res) => {
   
   try {
